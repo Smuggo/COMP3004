81a980757585f974ca49776a04a51c3efab3e642
Untamedhawk <janehring@gmail.com>
2015-01-30 09:29:01 -0500

    Setup Basic UI Work
    Title

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/a6/0059018a7da800141d34e1b1c44b345d
@@ -0,0 +1,26 @@
+package model;
+
+import javax.swing.JFrame;
+
+import view.MagicRealmWindow;
+import network.NetworkManager;
+import network.client.ClientApp;
+import network.server.ServerApp;
+
+public class MagicRealm {
+	
+	MagicRealmWindow lWindow;
+	NetworkManager lNetworkManager;
+	
+	public MagicRealm(){
+		lWindow = new MagicRealmWindow();
+		lNetworkManager = new NetworkManager();
+	}
+	
+	public void start(){
+		while(true){
+			
+			
+		}
+	}
+}
--- MagicRealm/src/model/MagicRealm.java
+++ MagicRealm/src/model/MagicRealm.java
@@ -2,18 +2,19 @@ package model;
 
 import javax.swing.JFrame;
 
-import view.MagicRealmWindow;
+import view.ViewManager;
+import view.externals.MagicRealmWindow;
 import network.NetworkManager;
 import network.client.ClientApp;
 import network.server.ServerApp;
 
 public class MagicRealm {
 	
-	MagicRealmWindow lWindow;
+	ViewManager lViewManager;
 	NetworkManager lNetworkManager;
 	
 	public MagicRealm(){
-		lWindow = new MagicRealmWindow();
+		lViewManager = new ViewManager();
 		lNetworkManager = new NetworkManager();
 	}
 	
