e08c1367ca98cad26a266c78296fdbe4b176ea63
Zacxter <zacharycarbonneau@gmail.com>
2015-02-12 16:38:18 -0500

    Hextiles on board and rotated.

--- MagicRealm/src/game/environment/hex/HexGrid.java
+++ MagicRealm/src/game/environment/hex/HexGrid.java
@@ -3,6 +3,7 @@ package game.environment.hex;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.util.ArrayList;
 
 public class HexGrid {
 	
@@ -11,15 +12,28 @@ public class HexGrid {
 	private int lRadius;
 	private Dimension lCanvasSize;
 	
-	public HexGrid(int aRadius, Dimension aCanvasSize){
+	public HexGrid(int aRadius, Dimension aCanvasSize, ArrayList<Hextile> hextiles){
 		lRadius = aRadius;
-		lGrid = new Hex[(2*aRadius)+1][(2*aRadius)+1];
+		int lGridSize = (2*aRadius)+1;
+		lGrid = new Hex[lGridSize][lGridSize];
 		
-		for(int y = 0; y < (2*aRadius)+1; y++){
-			for(int x = 0; x < (2*aRadius)+1; x++){
-				lGrid[x][y] = new Hex(x-lRadius,y-lRadius, aCanvasSize);
-				if(x == (aRadius) && y == (aRadius)){
-					lCenterHex = lGrid[x][y];
+		for(int y = 0; y < lGridSize; y++){
+			for(int x = 0; x < lGridSize; x++){
+				lGrid[x][y] = null;
+				for(int i = 0; i < hextiles.size(); i++){
+					// Find the hextile logic and create a hex
+					if (hextiles.get(i).getXLocation() == x-lRadius &&
+						hextiles.get(i).getYLocation() == y-lRadius) {
+						//hextiles.get(i).print();
+						//System.out.println("Success @ x = " +hextiles.get(i).getXLocation()+ " & y = " +hextiles.get(i).getYLocation());
+				
+						lGrid[x][y] = new Hex(x-lRadius,y-lRadius, aCanvasSize, hextiles.get(i));
+					}
+					
+					// Set Center Hex
+					if(x == (aRadius) && y == (aRadius)){
+						lCenterHex = lGrid[x][y];
+					}
 				}
 			}
 		}
@@ -35,14 +49,18 @@ public class HexGrid {
 	public void drawGrid(Graphics g, Dimension aCanvasSize, Point aMouse){
 		for(int y = -lRadius; y <= lRadius; y++ ){
 			for(int x = -lRadius; x <= lRadius; x++){
-				getHex(x,y).drawHex(x,y,g, aCanvasSize, aMouse);
+				if (getHex(x,y) != null) {
+					getHex(x,y).drawHex(x,y,g, aCanvasSize, aMouse);
+				}
 			}
 		}
 		
 		//DrawSelected
 		for(int y = -lRadius; y <= lRadius; y++ ){
 			for(int x = -lRadius; x <= lRadius; x++){
-				getHex(x,y).drawSelectedHex(x,y,g, aCanvasSize, aMouse);
+				if (getHex(x,y) != null) {
+					getHex(x,y).drawHex(x,y,g, aCanvasSize, aMouse);
+				}
 			}
 		}
 	}
