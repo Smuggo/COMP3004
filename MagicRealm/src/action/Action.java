85d4b864691c80b1c098634ded021da992553588
Sebastian Welch <sebastianwelch@gmail.com>
2015-02-21 18:29:18 -0500

    Added Action.java to the repo again

---
+++ MagicRealm/src/action/Action.java
@@ -0,0 +1,82 @@
+package action;
+
+import java.io.Serializable;
+import java.util.Random;
+
+import game.environment.hex.Clearing;
+import config.Config.ActionType;
+import config.Config.SearchType;
+
+public class Action implements Serializable {
+	/**
+  * 
+  */
+	private static final long serialVersionUID = -4900065088380619703L;
+
+	private ActionType lActionType;
+
+	private int cost;
+
+	// Move
+	private Clearing lClearingStart;
+	private Clearing lClearingEnd;
+
+	// Hide
+
+	// Rest
+
+	// Search
+	private SearchType lSearchType;
+
+	// Do not make Movement actions with this constructor
+	public Action() {
+
+	}
+
+	public void createMoveAction(Clearing aClearingStart, Clearing aClearingEnd) {
+		lActionType = ActionType.MOVE;
+		cost = 1;
+		lClearingStart = aClearingStart;
+		lClearingEnd = aClearingEnd;
+	}
+
+	public void createHideAction() {
+		lActionType = ActionType.HIDE;
+		cost = 1;
+	}
+
+	public void createRestAction() {
+		lActionType = ActionType.REST;
+		cost = 1;
+	}
+
+	public void createSearchAction(Clearing aClearingStart) {
+		lClearingStart = aClearingStart;
+		lActionType = ActionType.SEARCH;
+		cost = 1;
+	}
+
+	public int getCost() {
+		return cost;
+	}
+
+	public ActionType getActionType() {
+		return lActionType;
+	}
+
+	public Clearing getClearingStart() {
+		return lClearingStart;
+	}
+
+	public Clearing getClearingEnd() {
+		return lClearingEnd;
+	}
+
+	public void setSearchType(SearchType aSearchType) {
+		lSearchType = aSearchType;
+	}
+	
+	public SearchType getSearchType() {
+		return lSearchType;
+	}
+}
