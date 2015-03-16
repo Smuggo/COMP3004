6acb8f720f649fc1a6243905799a587f9cf6c208
Smuggo <sebastianwelch@gmail.com>
2015-03-16 16:36:57 -0400

    Added ArmourFactory

---
+++ MagicRealm/src/game/item/ArmourFactory.java
@@ -0,0 +1,14 @@
+package game.item;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import config.Config.ArmourType;
+
+public class ArmourFactory {
+	private Map<ArmourType, Armour> lArmourMap = new HashMap<ArmourType, Armour>();
+	
+	public ArmourFactory(){
+		lArmourMap.put(key, value)
+	}
+}
